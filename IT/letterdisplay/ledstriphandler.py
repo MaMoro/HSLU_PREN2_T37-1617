cb79e5a3f5140a3d4193795ced65f67d9f1e7e40
Marco Moro <dev@marcomoro.ch>
2017-03-11 10:47:06 +0100

    added PowerLED handling

--- IT/letterdisplay/ledstriphandler.py
+++ IT/letterdisplay/ledstriphandler.py
@@ -5,7 +5,7 @@
 # AUTHOR          : Moro Marco I.BSCI_F14.1301 <marco.moro@stud.hslu.ch>
 # DATE            : 03.02.2017
 # USAGE           :
-# VERSION         : 0.1
+# VERSION         : 0.2
 # USAGE           :
 # NOTES           :
 # PYTHON_VERSION  : 3.4.2
@@ -21,7 +21,8 @@ from logging.config import fileConfig
 class LEDStripHandler:
 
     __gpio_init = False
-    __ledpins = [11, 12, 13, 15, 16]
+    __letterledpins = [11, 12, 13, 15, 16]
+    __powerledpin = 14
     fileConfig(cfg.get_logging_config_fullpath())
     __log = logging.getLogger()
     __log.setLevel(cfg.get_settings_loglevel())
@@ -30,16 +31,33 @@ class LEDStripHandler:
     def display_letter_on_LEDs(number):
         if not LEDStripHandler.__gpio_init:
             LEDStripHandler.__setupGPIOPins()
-        GPIO.output(LEDStripHandler.__ledpins[:number], GPIO.HIGH)  # LEDs einschalten
+        GPIO.output(LEDStripHandler.__letterledpins[:number], GPIO.HIGH)  # LEDs einschalten
         time.sleep(0.1)  # Pin "setzen lassen"
         LEDStripHandler.__log.info("LEDs for number " + str(number) + " turned on!")
+
+    @staticmethod
+    def start_powerled():
+        if not LEDStripHandler.__gpio_init:
+            LEDStripHandler.__setupGPIOPins()
+        GPIO.output(LEDStripHandler.__powerledpin, GPIO.HIGH)
+        time.sleep(0.1)
+        LEDStripHandler.__log.info("PowerLED turned on!")
+
+    @staticmethod
+    def stop_powerled():
+        if not LEDStripHandler.__gpio_init:
+            LEDStripHandler.__setupGPIOPins()
+        GPIO.output(LEDStripHandler.__powerledpin, GPIO.LOW)
+        time.sleep(0.1)
+        LEDStripHandler.__log.info("PowerLED turned off!")
 
     @staticmethod
     def __setupGPIOPins():
         LEDStripHandler.__log.info("Initialize GPIO Pins...")
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)    #GPIO Layout (Pin-Nummer verwenden)
-        GPIO.setup(LEDStripHandler.__ledpins, GPIO.OUT, initial=GPIO.LOW) #declare all Pins as output and turn LEDs off
-        time.sleep(0.1)             #Pin "setzen lassen"
+        GPIO.setup(LEDStripHandler.__letterledpins, GPIO.OUT, initial=GPIO.LOW) #declare all LED Pins as output and turn LEDs off
+        GPIO.setup(LEDStripHandler.__powerledpin, GPIO.OUT, initial=GPIO.LOW)   #declare PowerLED Pin as output and turn LED off
+        time.sleep(0.1)             #Pins "setzen lassen"
         LEDStripHandler.__gpio_init = True
         LEDStripHandler.__log.info("Initialize GPIO Pins done!")
