fed359bf06fa2aa99dbde320a654e0b66cf12a20
eve <eve.meier@stud.hslu.ch>
2017-03-17 11:21:11 +0100

    added some more notes and comments

--- IT/main.py
+++ IT/main.py
@@ -17,18 +17,41 @@ def main():
     print("* Autonomes Geländefahrzeug: PREN Team 37, 2017  *")
     print("**************************************************")
 
+#Import all needed classes
+
+    from common.communication.communicationhandler import SerialCommunicationHandler
+    from common.processing.camerahandler import CameraHandler
+    from letterdetection.letterdetectionhandler import LetterDetectionHandler
+    from trafficlight.trafficlightdetectionhandler import TrafficLightDetection
+    from letterdisplay.ledstriphandler import LEDStripHandler
 
     #Load configuration / config handler
     #Init Webserver
     #Wait for parcour setting (left/right) - new site?
 
     #Init communication between raspi and freedom
-    #Init needed Components.... Motor? Cervo?
-    #Init camera and power LED
+
+    #Init camera
+    #CameraHandler() ??
 
     #Traffic Light Detection
+    #TrafficLightDetection.detect_trafficlight(self, frame) needs frame!
+    #TODO: Define how frame is delivered to detect_trafficlight
+    #mainTrafficLightDetection = TrafficLightDetection()
+    #mainTrafficLightDetection.detect_trafficlight(self, frame)
+
+    #Init PowerLED
+    #LEDStripHandler.start_powerled()
 
     #Letter Detection
+    #LetterDetectionHandler()
+    #Somehow number = LetterDetectionHandler()
+
+    #Stop PowerLED
+    #LEDStripHandler.stop_powerled()
+
+    #Display Letter on LEDStrip
+    #LEDStripHandler.display_letter_on_LEDs(number)
 
 
 # ================================================================================
