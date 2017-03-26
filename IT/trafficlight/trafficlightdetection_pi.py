26797c292d7b83fe5b6640fccb6949d3e9a39518
Marco Moro <dev@marcomoro.ch>
2017-03-26 20:33:29 +0200

    made TrafficLightPi threadsafe

--- IT/trafficlight/trafficlightdetection_pi.py
+++ IT/trafficlight/trafficlightdetection_pi.py
@@ -14,67 +14,55 @@
 import cv2
 import time
 import logging
-import threading
 import common.config.confighandler as cfg
 
 from .trafficlightdetectionhandler import TrafficLightDetection
 from logging.config import fileConfig
 from common.processing.camerahandler import CameraHandler
+from threading import Thread
 
 
 class TrafficLightDetectionPi(object):
-
-    camera = None
-    rawCapture = None
-    stream = None
-    frame = None
-    thread = None
-    last_access = 0
-    stopped = False
-
     # Initialize the class
     def __init__(self):
         fileConfig(cfg.get_logging_config_fullpath())
         self.__log = logging.getLogger()
+        self.__log.setLevel(cfg.get_settings_loglevel())
+        self.stopped = True
+        self.frame = None
+        self.detectionres = None
+        self.trafficstatus = "red"
+        self.pistream = CameraHandler().start()
         self.start()
 
     def start(self):
-        if self.thread is None:
-            self.thread = threading.Thread(target=self._thread)
-            self.last_access = time.time()
-            self.thread.start()
-
-            while self.frame is None:
-                time.sleep(0)
+        if self.stopped:
+            t = Thread(target=self.update, args=())
+            t.daemon = True
+            self.stopped = False
+            t.start()
+            time.sleep(0.5)
 
     # Get a frame
     def get_frame(self):
-        self.last_access = time.time()
-        self.start()
+        if self.stopped:
+            self.start()
+
         return self.frame
 
-    @classmethod
-    def _thread(cls):
-        cls.last_access = time.time()
-        if cls.camera is None or cls.rawCapture is None or cls.stream is None:
-            cls.camera = CameraHandler().get_pi_camerainstance()
-            cls.rawCapture = CameraHandler().get_pi_rgbarray()
-            cls.stream = cls.camera.capture_continuous(cls.rawCapture, format="bgr", use_video_port=True)
-        for f in cls.stream:
-            image = f.array
-            tld = TrafficLightDetection()
-            data = tld.detect_trafficlight(image)
-            _, jpeg = cv2.imencode('.jpg', data)
-            cls.frame = jpeg.tobytes()
-            cls.rawCapture.truncate(0)
+    def getstatus(self):
+        return self.trafficstatus
 
-            t = time.time()
-            if cls.stopped or t - cls.last_access > 20:
-                cls.stream.close()
-                cls.rawCapture.close()
-                CameraHandler().stop()
-                break
-        cls.thread = None
+    def updatestatus(self, status):
+        self.trafficstatus = status
+
+    def update(self):
+        while not self.stopped:
+            img = self.pistream.read()
+            tld = TrafficLightDetection()
+            self.detectionres = tld.detect_trafficlight(img)
+            self.updatestatus(tld.get_green_counter())
 
     def stop(self):
         self.stopped = True
+        CameraHandler().stop()
