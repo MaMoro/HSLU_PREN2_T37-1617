2730037e402ca26ad0b2a28f98544084f9abd676
Marco Moro <dev@marcomoro.ch>
2017-04-07 17:45:09 +0200

    changed test

--- IT/letterdisplay/test_ledstrip.py
+++ IT/letterdisplay/test_ledstrip.py
@@ -1,11 +1,21 @@
 import time
+
+from RPi import GPIO
 from letterdisplay.ledstriphandler import LEDStripHandler
 
-LEDStripHandler.display_letter_on_LEDs(1)
-LEDStripHandler.display_letter_on_LEDs(2)
-LEDStripHandler.display_letter_on_LEDs(3)
-LEDStripHandler.display_letter_on_LEDs(4)
-LEDStripHandler.display_letter_on_LEDs(5)
-LEDStripHandler.start_powerled()
-time.sleep(0.01)
-LEDStripHandler.stop_powerled()
+__letterledpins = [11, 12, 13, 15, 16]
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)  # GPIO Layout (Pin-Nummer verwenden)
+GPIO.setup(__letterledpins, GPIO.OUT,
+           initial=GPIO.LOW)  # declare all LED Pins as output and turn LEDs off
+# GPIO.output(16, GPIO.HIGH)
+
+
+# LEDStripHandler.display_letter_on_LEDs(1)
+# LEDStripHandler.display_letter_on_LEDs(2)
+# LEDStripHandler.display_letter_on_LEDs(3)
+# LEDStripHandler.display_letter_on_LEDs(4)
+# LEDStripHandler.display_letter_on_LEDs(5)
+# LEDStripHandler.start_powerled()
+time.sleep(60)
+# LEDStripHandler.stop_powerled()
