7eba2da17177c603b3c27f23e38f1bffedf7a1bf
Marco Moro <dev@marcomoro.ch>
2017-05-04 11:39:58 +0200

    testing LED in production

--- IT/testing/test_ledstrip.py
+++ IT/testing/test_ledstrip.py
@@ -6,16 +6,14 @@ from letterdisplay.ledstriphandler import LEDStripHandler
 __letterledpins = [11, 12, 13, 15, 16]
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)  # GPIO Layout (Pin-Nummer verwenden)
-GPIO.setup(__letterledpins, GPIO.OUT,
-           initial=GPIO.LOW)  # declare all LED Pins as output and turn LEDs off
-# GPIO.output(16, GPIO.HIGH)
+GPIO.setup(__letterledpins, GPIO.OUT, initial=GPIO.LOW)  # declare all LED Pins as output and turn LEDs off
 
-
-# LEDStripHandler.display_letter_on_LEDs(1)
-# LEDStripHandler.display_letter_on_LEDs(2)
-# LEDStripHandler.display_letter_on_LEDs(3)
-# LEDStripHandler.display_letter_on_LEDs(4)
-# LEDStripHandler.display_letter_on_LEDs(5)
-# LEDStripHandler.start_powerled()
+LEDStripHandler.display_letter_on_LEDs(1)
+LEDStripHandler.display_letter_on_LEDs(2)
+LEDStripHandler.display_letter_on_LEDs(3)
+LEDStripHandler.display_letter_on_LEDs(4)
+LEDStripHandler.display_letter_on_LEDs(5)
+LEDStripHandler.start_powerled()
+time.sleep(8)
+LEDStripHandler.stop_powerled()
 time.sleep(60)
-# LEDStripHandler.stop_powerled()
