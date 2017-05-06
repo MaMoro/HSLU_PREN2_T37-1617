3292c559259b04cb88db49c1751c5e64de15fad2
Marco Moro <dev@marcomoro.ch>
2017-05-06 21:25:44 +0200

    fixed autostart for production ->running & tested!

--- IT/autostart.sh
+++ IT/autostart.sh
@@ -1,18 +1,15 @@
+#!/bin/bash
 # =======================================================================
 # title           :autostart.sh
 # description     :This program enables to start project at startup
 # author          :Marco Moro
 # date            :30.01.2016
 # version         :0.1
-# usage           :sudo autostart.sh
+# usage           :autostart.sh
 # notes           :
 # python_version  :3.4.2
 # opencv_version  :3.1.0
 # =======================================================================
 
-#!/bin/bash
-FILEPATH=/home/pi/Desktop
-# !!!!!!!!!!!!!!! start this script with sudo !!!!!!!!!!!!!!!
-
-sleep 3  # wait 3s to let RPi finish the start up procedure
-python3 $FILEPATH/PREN/main.py > $FILEPATH/logging/run.log 2>&1 &
+sleep 3  # wait 5s to let RPi finish the start up procedure
+/home/pi/.virtualenvs/cv/bin/python3 /home/pi/Desktop/PREN/main.py &
