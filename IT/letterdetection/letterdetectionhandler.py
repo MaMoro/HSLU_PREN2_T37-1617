a5b1b9d37af7128154a4c19062b48f7f5a9a89bd
Marco Moro <dev@marcomoro.ch>
2017-06-06 09:20:48 +0200

    segregated initcamera for performance improvements

--- IT/letterdetection/letterdetectionhandler.py
+++ IT/letterdetection/letterdetectionhandler.py
@@ -40,13 +40,13 @@ class LetterDetectionHandler(object):
             self.__log.info("Letterdetection started")
             self.frame = None
             self.stopped = True
+            self.pistream = None
             self.numbertodisplay = 0
             self.processingqueue = 0
             self.resultqueue = 0
             self.num_processing_units = 4
             self.font = cfg.get_opencv_font()
             self.min_amount_processed_letters = cfg.get_letter_min_amount_processed_letters()
-            self.initqueues()
 
         def start(self):
             # start the thread to read frames from the video stream
@@ -61,7 +61,8 @@ class LetterDetectionHandler(object):
         def starttest(self):
             # start the thread to read frames from the video stream
             if self.stopped:
-                t = Thread(target=self.rundetection, args=())
+                self.initcamera()
+                t = Thread(target=self.processing_test, args=())
                 t.daemon = True
                 self.stopped = False
                 t.start()
@@ -71,12 +72,11 @@ class LetterDetectionHandler(object):
         def stop(self):
             self.stopped = True  # indicate that the thread should be stopped
 
-        def rundetection(self):
+        def processing_test(self):
             self.__log.info("Start capturing")
-            pistream = CameraHandler()
 
             while True:
-                self.frame = pistream.read()
+                self.frame = self.pistream.read()
                 redmask = ImageConverter.mask_color_red_fullhsv(self.frame)
                 imgmarked, edges = ImageAnalysis.get_ordered_corners_drawed(redmask, self.frame)
                 if edges != 0:
@@ -84,9 +84,9 @@ class LetterDetectionHandler(object):
                     cropped = ImageConverter.minimize_roi_lettercontour(correctedimg)
                     try:
                         numberimg = ImageAnalysis.get_roman_letter_drawed(cropped)
-                        number = ImageAnalysis.get_roman_letter(cropped)
-                        LEDStripHandler.display_letter_on_LEDs(number)
-                        CommunicationValues().send_letter(number)
+                        # number = ImageAnalysis.get_roman_letter(cropped)
+                        # LEDStripHandler.display_letter_on_LEDs(number)
+                        # CommunicationValues().send_letter(number)
 
                         cv2.imshow("Letter", numberimg)
                         cv2.imshow("Transformed", correctedimg)
@@ -106,65 +106,10 @@ class LetterDetectionHandler(object):
             CameraHandler().stop()
             cv2.destroyAllWindows()
 
-        def processing(self):
-            self.__log.info("Start processing, create Image-Processing-Units..")
-            num_units = 4
-            processingqueue = multiprocessing.JoinableQueue()
-            resultqueue = multiprocessing.Queue()
-
-            processingunits = [ImageProcessing(processingqueue, resultqueue) for i in range(num_units)]
-            for w in processingunits:
-                w.start()
-            self.__log.info("Image-Processing-Units created, ready to process...")
-            imgcount = 0
-            LEDStripHandler.start_powerled()
-            pistream = CameraHandler()
-
-            self.__log.info("Ready! Start capturing")
-
-            while True:
-                self.frame = pistream.read()
-                redmask = ImageConverter.mask_color_red_fullhsv(self.frame)
-                imgmarked, edges = ImageAnalysis.get_ordered_corners_drawed(redmask, self.frame)
-                if edges != 0:
-                    processingqueue.put(ImageNumber(self.frame, edges))
-                    imgcount += 1
-                elif imgcount > self.min_amount_processed_letters:
-                    # if more than specified images processed and no more edges found it's assumed that the number on the wall has passed
-                    for i in range(num_units):
-                        processingqueue.put(None)  # enforce ImageProcessing instances to terminate
-                    processingqueue.join()  # waiting for alle processes to be terminated
-                    break
-
-                #cv2.imshow("mask", redmask)
-                cv2.imshow("imagemarked", imgmarked)
-
-                key = cv2.waitKey(1) & 0xFF
-                if key == ord("q"):
-                    self.__log.info("Finished capturing")
-                    break
-
-            CameraHandler().stop()
-            cv2.destroyAllWindows()
-            self.__log.info("Processed " + str(imgcount) + " images")
-            allnumbers = []
-            print(resultqueue.qsize())
-            while resultqueue.qsize() != 0:
-                allnumbers.append(resultqueue.get())
-            numbertodisplay = ImageAnalysis.most_voted_number(allnumbers)
-            LEDStripHandler.display_letter_on_LEDs(numbertodisplay)
-            time.sleep(5)
-            LEDStripHandler.stop_powerled()
-            LEDStripHandler.turn_off_all_letter_LEDS()
-            # CommunicationValues().send_letter(numbertodisplay)
-
         def processing_prod(self):
             imgcount = 0
-            pistream = CameraHandler()
-            self.__log.info("Ready! Start capturing")
-
             while True:
-                self.frame = pistream.read()
+                self.frame = self.pistream.read()
                 redmask = ImageConverter.mask_color_red_fullhsv(self.frame)
                 edges = ImageAnalysis.get_ordered_corners(redmask)
                 if edges != 0:
@@ -193,6 +138,10 @@ class LetterDetectionHandler(object):
             for w in processingunits:
                 w.start()
             self.__log.info("Image-Processing-Units created, ready to process...")
+
+        def initcamera(self):
+            self.pistream = self.pistream = CameraHandler()
+            self.__log.info("Ready! Start capturing")
 
         def get_frame(self):
             return self.frame  # return the frame most recently read
@@ -223,6 +172,8 @@ if __name__ == '__main__':
     time.sleep(5)
     serialcomm.send_start()"""
     ldh = LetterDetectionHandler()
-    ldh.starttest()
-    #numbertodisplay = ldh.start()
-    #print("snömmerli esch: " + str(numbertodisplay))
+    # ldh.starttest()
+    ldh.initqueues()
+    ldh.initcamera()
+    numbertodisplay = ldh.start()
+    print("snömmerli esch: " + str(numbertodisplay))
